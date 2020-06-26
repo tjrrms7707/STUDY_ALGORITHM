efce498240434687aab357cada75b33562d2477d
GEUN <tjrrms7707@icloud.com>
2020-06-26 16:29:48 +0900

    백준 2163번 풀이 성공

--- src/type/dp/no_2163.java
+++ src/type/dp/no_2163.java
@@ -1,8 +1,14 @@
 package type.dp;
 
+import java.util.Scanner;
+
 public class no_2163 {
     public static void main(String[] args) {
-        //git Test
+        Scanner in = new Scanner(System.in);
+        int N = in.nextInt();
+        int M = in.nextInt();
+
+        System.out.println(N * M -1);
     }
 
 }
